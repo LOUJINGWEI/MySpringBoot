133a5f94bf7c4bc80b9d30c6fdae00febbe43f2a
AS娄 <593433839@qq.com>
2018-12-25 17:10:11 +0800

    first

---
+++ src/test/java/com/sailing/lou/LouApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sailing.lou;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LouApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
